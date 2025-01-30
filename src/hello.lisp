38ecaeaa191eb6abbca9d8226aeb770ef206e7a5
fantasma <carmodepp@gmail.com>
2025-01-29 22:26:27 -0300

    Lisp é difícil de aprender porém um pouco de dedicação, podemos aprende-la

---
+++ src/hello.lisp
@@ -0,0 +1,3 @@
+(defun main ()
+    (print "Hello, world!"))
+(main)
---
+++ src/index.lisp
@@ -0,0 +1,9 @@
+; Variáveis global a primeira é a "defvar".
+
+(defvar *nome* "João felipe")
+(print *nome*)
+
+; E variáveis global a segunda é a "defparameter".
+
+(defparameter *idade* 25)
+(print *idade*)
